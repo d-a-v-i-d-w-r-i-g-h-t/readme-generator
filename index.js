5d2d84e3ad94a8af32da09269a0f99a6b2a1fb98
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-17 15:31:10 -0700

    Add questions

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const questions = [
     "What is your project's name?",
     "Please write a short description of your project:",
     "What kind of license should your project have?",
-    ""
+    "What command should be run to install dependencies?",
+    "What command should be run to run tests?",
+    "What does the user need to know about using the repo?",
+    "What does the user need to know about contributing to the repo?"
 ];
 
 // TODO: Create a function to write README file
